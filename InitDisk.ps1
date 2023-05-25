5207196f69823a5df40b7ae4bcce7901611a1e90
noaxxess <113463394+noaxxess@users.noreply.github.com>
2023-05-25 14:38:48 -0400

    Changed DskLtr to DrvLtr

---
+++ InitDisk.ps1
@@ -0,0 +1,12 @@
+﻿param (
+	#Disk Letter
+	[Parameter(ValueFromPipeline = $true, Mandatory=$true, HelpMessage = "Drive Letter")]
+	[String]$DrvLtr
+)
+
+#Initialize Data Disk on Local Machine
+$DiskNum=(Get-Disk | Where PartitionStyle -eq 'raw').Number
+
+Initialize-Disk -Number $diskNum -PartitionStyle MBR
+
+New-Partition -DiskNumber $diskNum -UseMaximumSize -DriveLetter $DskLtr | Format-Volume -FileSystem NTFS -NewFileSystemLabel 'NTDS' -Confirm:$false
