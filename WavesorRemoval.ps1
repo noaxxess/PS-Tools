14417df400e81014e169810379cafebdcf57d93c
noaxxess <awhite@alwaysuptech.com>
2023-07-12 08:35:13 -0400

    Added Output to Show if Keys Detected Removed aliases and replaced with powershell commands

--- WavesorRemoval.ps1
+++ WavesorRemoval.ps1
@@ -9,129 +9,147 @@ if($PSBoundParameters.ContainsKey('LogFile') -eq $false){
 
 Start-Transcript -Path $LogFile -NoClobber
 
+Write-Host "Stopping Wavesor Processes"
 Get-Process wavebrowser -ErrorAction SilentlyContinue | Stop-Process -Force
 Get-Process SWUpdater -ErrorAction SilentlyContinue | Stop-Process -Force
-sleep 2
+Start-Sleep 2
 
 $user_list = Get-Item C:\users\* | Select-Object Name -ExpandProperty Name
-foreach ($i in $user_list) {
-    if ($i -notlike "*Public*") {
-        $exists = test-path -path "C:\users\$i\Wavesor Software"
+foreach ($User in $user_list) {
+    if ($User -notlike "*Public*") {
+        $exists = test-path -path "C:\users\$user\Wavesor Software"
         if ($exists -eq $True) {
-            rm "C:\users\$i\Wavesor Software" -Force -Recurse -ErrorAction SilentlyContinue
-            $exists = test-path -path "C:\users\$i\Wavesor Software"
+            Write-Host "Found C:\users\$user\Wavesor Software. Removing..."
+            Remove-Item "C:\users\$user\Wavesor Software" -Force -Recurse -ErrorAction SilentlyContinue
+            $exists = test-path -path "C:\users\$user\Wavesor Software"
             if ($exists -eq $True) {
-                "WaveBrowser Removal Unsuccessful => C:\users\$i\Wavesor Software"
+                "WaveBrowser Removal Unsuccessful => C:\users\$user\Wavesor Software"
             }
         }
-        $exists = test-path -path "C:\users\$i\WebNavigatorBrowser"
+        $exists = test-path -path "C:\users\$user\WebNavigatorBrowser"
         if ($exists -eq $True) {
-            rm "C:\users\$i\WebNavigatorBrowser" -Force -Recurse -ErrorAction SilentlyContinue
-            $exists = test-path -path "C:\users\$i\WebNavigatorBrowser"
+            Write-Host "Found C:\users\$user\WebNavigatorBrowser. Removing..."
+            Remove-Item "C:\users\$user\WebNavigatorBrowser" -Force -Recurse -ErrorAction SilentlyContinue
+            $exists = test-path -path "C:\users\$user\WebNavigatorBrowser"
             if ($exists -eq $True) {
-                "WaveBrowser Removal Unsuccessful => C:\users\$i\WebNavigatorBrowser"
+                "WaveBrowser Removal Unsuccessful => C:\users\$user\WebNavigatorBrowser"
             }
         }
-        $exists = test-path -path "C:\users\$i\appdata\local\WaveBrowser"
+        $exists = test-path -path "C:\users\$user\appdata\local\WaveBrowser"
         if ($exists -eq $True) {
-            rm "C:\users\$i\appdata\local\WaveBrowser" -Force -Recurse -ErrorAction SilentlyContinue
-            $exists = test-path -path "C:\users\$i\appdata\local\WaveBrowser"
+            Write-Host "Found C:\users\$user\appdata\local\WaveBrowser. Removing..."
+            Remove-Item "C:\users\$user\appdata\local\WaveBrowser" -Force -Recurse -ErrorAction SilentlyContinue
+            $exists = test-path -path "C:\users\$user\appdata\local\WaveBrowser"
             if ($exists -eq $True) {
-                "WaveBrowser Removal Unsuccessful => C:\users\$i\appdata\local\WaveBrowser"
+                "WaveBrowser Removal Unsuccessful => C:\users\$user\appdata\local\WaveBrowser"
             }
         }
-        $exists = test-path -path "C:\users\$i\appdata\local\WebNavigatorBrowser"
+        $exists = test-path -path "C:\users\$user\appdata\local\WebNavigatorBrowser"
         if ($exists -eq $True) {
-            rm "C:\users\$i\appdata\local\WebNavigatorBrowser" -Force -Recurse -ErrorAction SilentlyContinue
-            $exists = test-path -path "C:\users\$i\appdata\local\WebNavigatorBrowser"
+            Write-Host "Found C:\users\$user\appdata\local\WebNavigatorBrowser. Removing..."
+            Remove-Item "C:\users\$user\appdata\local\WebNavigatorBrowser" -Force -Recurse -ErrorAction SilentlyContinue
+            $exists = test-path -path "C:\users\$user\appdata\local\WebNavigatorBrowser"
             if ($exists -eq $True) {
-                "WaveBrowser Removal Unsuccessful => C:\users\$i\appdata\local\WebNavigatorBrowser"
+                "WaveBrowser Removal Unsuccessful => C:\users\$user\appdata\local\WebNavigatorBrowser"
             }
         }
-        rm "C:\users\$i\downloads\Wave Browser*.exe" -Force -ErrorAction SilentlyContinue
-        rm "C:\users\$i\appdata\roaming\microsoft\windows\start menu\programs\WaveBrowser.lnk" -Force -ErrorAction SilentlyContinue
-        rm "C:\USERS\$i\APPDATA\ROAMING\MICROSOFT\INTERNET EXPLORER\QUICK LAUNCH\WAVEBROWSER.LNK" -ErrorAction SilentlyContinue
-        rm "C:\USERS\$i\DESKTOP\WAVEBROWSER.LNK" -ErrorAction SilentlyContinue
+        Remove-Item "C:\users\$user\downloads\Wave Browser*.exe" -Force -ErrorAction SilentlyContinue
+        Remove-Item "C:\users\$user\appdata\roaming\microsoft\windows\start menu\programs\WaveBrowser.lnk" -Force -ErrorAction SilentlyContinue
+        Remove-Item "C:\USERS\$user\APPDATA\ROAMING\MICROSOFT\INTERNET EXPLORER\QUICK LAUNCH\WAVEBROWSER.LNK" -ErrorAction SilentlyContinue
+        Remove-Item "C:\USERS\$user\DESKTOP\WAVEBROWSER.LNK" -ErrorAction SilentlyContinue
     }
 }
 
 $tasks = Get-ScheduledTask -TaskName *Wave* | Select-Object -ExpandProperty TaskName
-foreach ($i in $tasks) {
-	Unregister-ScheduledTask -TaskName $i -Confirm:$false -ErrorAction SilentlyContinue
+$taskCount=0
+foreach ($task in $tasks) {
+	Unregister-ScheduledTask -TaskName $task -Confirm:$false -ErrorAction SilentlyContinue
+    $taskCount++
 }
 
+Write-Host $taskCount + " scheduled tasks removed."
+
+Write-Host "Cleaning Registry..."
+
 Remove-Item -Path 'Registry::HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\TREE\Wave*' -Recurse -ErrorAction SilentlyContinue
-Remove-Item -Path "C:\windows\system32\tasks\Wavesor*" -Recurse -Confirm:$false -ErrorAction SilentlyContinue
+Remove-Item -Path "C:\Windows\system32\tasks\Wavesor*" -Recurse -Confirm:$false -ErrorAction SilentlyContinue
 
 $sid_list = Get-Item -Path "Registry::HKU\*" | Select-String -Pattern "S-\d-(?:\d+-){5,14}\d+"
-foreach ($i in $sid_list) {
-    if ($i -notlike "*_Classes*") {
-        $keyexists = test-path -path "Registry::$i\Software\WaveBrowser"
+foreach ($sid in $sid_list) {
+    if ($sid -notlike "*_Classes*") {
+        $keyexists = test-path -path "Registry::$sid\Software\WaveBrowser"
         if ($keyexists -eq $True) {
-            Remove-Item -Path "Registry::$i\Software\WaveBrowser" -Recurse -ErrorAction SilentlyContinue
-            $keyexists = test-path -path "Registry::$i\Software\WaveBrowser"
+            Write-Host "Key Registry::$sid\Software\WaveBrowser Exists. Removing Key"
+            Remove-Item -Path "Registry::$sid\Software\WaveBrowser" -Recurse -ErrorAction SilentlyContinue
+            $keyexists = test-path -path "Registry::$sid\Software\WaveBrowser"
             if ($keyexists -eq $True) {
-                "WaveBrowser Removal Unsuccessful => Registry::$i\Software\WaveBrowser"
+                "WaveBrowser Removal Unsuccessful => Registry::$sid\Software\WaveBrowser"
             }
         }
-        $keyexists = test-path -path "Registry::$i\Software\Wavesor"
+        $keyexists = test-path -path "Registry::$sid\Software\Wavesor"
         if ($keyexists -eq $True) {
-            Remove-Item -Path "Registry::$i\Software\Wavesor" -Recurse -ErrorAction SilentlyContinue
-            $keyexists = test-path -path "Registry::$i\Software\Wavesor"
+            Write-Host "Key Registry::$sid\Software\Wavesor Exists. Removing Key"
+            Remove-Item -Path "Registry::$sid\Software\Wavesor" -Recurse -ErrorAction SilentlyContinue
+            $keyexists = test-path -path "Registry::$sid\Software\Wavesor"
             if ($keyexists -eq $True) {
-                "WaveBrowser Removal Unsuccessful => Registry::$i\Software\Wavesor"
+                "WaveBrowser Removal Unsuccessful => Registry::$sid\Software\Wavesor"
             }
         }
-        $keyexists = test-path -path "Registry::$i\Software\WebNavigatorBrowser"
+        $keyexists = test-path -path "Registry::$sid\Software\WebNavigatorBrowser"
         if ($keyexists -eq $True) {
-            Remove-Item -Path "Registry::$i\Software\WebNavigatorBrowser" -Recurse -ErrorAction SilentlyContinue
-            $keyexists = test-path -path "Registry::$i\Software\WebNavigatorBrowser"
+            Write-Host "Key Registry::$sid\Software\WebNavigatorBrowser Exists. Removing Key."
+            Remove-Item -Path "Registry::$sid\Software\WebNavigatorBrowser" -Recurse -ErrorAction SilentlyContinue
+            $keyexists = test-path -path "Registry::$sid\Software\WebNavigatorBrowser"
             if ($keyexists -eq $True) {
-                "WaveBrowser Removal Unsuccessful => Registry::$i\Software\WebNavigatorBrowser"
+                "WaveBrowser Removal Unsuccessful => Registry::$sid\Software\WebNavigatorBrowser"
             }
         }
-        $keyexists = test-path -path "Registry::$i\Software\Microsoft\Windows\CurrentVersion\Uninstall\WaveBrowser"
+        $keyexists = test-path -path "Registry::$sid\Software\Microsoft\Windows\CurrentVersion\Uninstall\WaveBrowser"
         if ($keyexists -eq $True) {
-            Remove-Item -Path "Registry::$i\Software\Microsoft\Windows\CurrentVersion\Uninstall\WaveBrowser" -Recurse -ErrorAction SilentlyContinue
-            $keyexists = test-path -path "Registry::$i\Software\Microsoft\Windows\CurrentVersion\Uninstall\WaveBrowser"
+            Write-Host "Key Registry::$sid\Software\Microsoft\Windows\CurrentVersion\Uninstall\WaveBrowser Exists. Removing Key."
+            Remove-Item -Path "Registry::$sid\Software\Microsoft\Windows\CurrentVersion\Uninstall\WaveBrowser" -Recurse -ErrorAction SilentlyContinue
+            $keyexists = test-path -path "Registry::$sid\Software\Microsoft\Windows\CurrentVersion\Uninstall\WaveBrowser"
             if ($keyexists -eq $True) {
-                "WaveBrowser Removal Unsuccessful => Registry::$i\Software\Microsoft\Windows\CurrentVersion\Uninstall\WaveBrowser"
+                "WaveBrowser Removal Unsuccessful => Registry::$sid\Software\Microsoft\Windows\CurrentVersion\Uninstall\WaveBrowser"
             }
         }
-        $keyexists = test-path -path "Registry::$i\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\WaveBrowser"
+        $keyexists = test-path -path "Registry::$sid\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\WaveBrowser"
         if ($keyexists -eq $True) {
-            Remove-Item -Path "Registry::$i\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\WaveBrowser" -Recurse -ErrorAction SilentlyContinue
-            $keyexists = test-path -path "Registry::$i\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\WaveBrowser"
+            Write-Host "Key Registry::$sid\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\WaveBrowser Exists. Removing Key"
+            Remove-Item -Path "Registry::$sid\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\WaveBrowser" -Recurse -ErrorAction SilentlyContinue
+            $keyexists = test-path -path "Registry::$sid\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\WaveBrowser"
             if ($keyexists -eq $True) {
-                "WaveBrowser Removal Unsuccessful => Registry::$i\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\WaveBrowser"
+                "WaveBrowser Removal Unsuccessful => Registry::$sid\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\WaveBrowser"
             }
         }
-        $keypath = "Registry::$i\Software\Microsoft\Windows\CurrentVersion\Run"
+        $keypath = "Registry::$sid\Software\Microsoft\Windows\CurrentVersion\Run"
         $keyexists = (Get-Item $keypath).Property -contains "Wavesor SWUpdater"
         if ($keyexists -eq $True) {
-            Remove-ItemProperty -Path "Registry::$i\Software\Microsoft\Windows\CurrentVersion\Run" -Name "Wavesor SWUpdater" -ErrorAction SilentlyContinue
+            Write-Host "Wavesor Updater Found in Registry. Removing Registry Key."
+            Remove-ItemProperty -Path "Registry::$sid\Software\Microsoft\Windows\CurrentVersion\Run" -Name "Wavesor SWUpdater" -ErrorAction SilentlyContinue
             $keyexists = (Get-Item $keypath).Property -contains "Wavesor SWUpdater"
             if ($keyexists -eq $True) {
-                "WaveBrowser Removal Unsuccessful => Registry::$i\Software\Microsoft\Windows\CurrentVersion\Run.Wavesor SWUpdater"
+                "WaveBrowser Removal Unsuccessful => Registry::$sid\Software\Microsoft\Windows\CurrentVersion\Run.Wavesor SWUpdater"
             }
         }
     }
 }
 
 $sid_list = Get-Item -Path "Registry::HKU\*" | Select-String -Pattern "S-\d-(?:\d+-){5,14}\d+"
-foreach ($i in $sid_list) {
-    if ($i -like "*_Classes*") {
-        remove-item "Registry::$i\WaveBrwsHTM*" -Recurse -ErrorAction SilentlyContinue
-        remove-item "Registry::$i\WavesorSWUpdater.CredentialDialogUser" -Recurse -ErrorAction SilentlyContinue
-        remove-item "Registry::$i\WavesorSWUpdater.CredentialDialogUser.1.0" -Recurse -ErrorAction SilentlyContinue
-        remove-item "Registry::$i\WavesorSWUpdater.OnDemandCOMClassUser" -Recurse -ErrorAction SilentlyContinue
-        remove-item "Registry::$i\WavesorSWUpdater.OnDemandCOMClassUser.1.0" -Recurse -ErrorAction SilentlyContinue
-        remove-item "Registry::$i\WavesorSWUpdater.PolicyStatusUser" -Recurse -ErrorAction SilentlyContinue
-        remove-item "Registry::$i\WavesorSWUpdater.PolicyStatusUser.1.0" -Recurse -ErrorAction SilentlyContinue
-        remove-item "Registry::$i\WavesorSWUpdater.Update3COMClassUser" -Recurse -ErrorAction SilentlyContinue
-        remove-item "Registry::$i\WavesorSWUpdater.Update3COMClassUser.1.0" -Recurse -ErrorAction SilentlyContinue
-        remove-item "Registry::$i\WavesorSWUpdater.Update3WebUser" -Recurse -ErrorAction SilentlyContinue
-        remove-item "Registry::$i\WavesorSWUpdater.Update3WebUser.1.0" -Recurse -ErrorAction SilentlyContinue
+foreach ($sid in $sid_list) {
+    if ($sid -like "*_Classes*") {
+        Write-Host $sid + " Located. Removing Registry Keys"
+        remove-item "Registry::$sid\WaveBrwsHTM*" -Recurse -ErrorAction SilentlyContinue
+        remove-item "Registry::$sid\WavesorSWUpdater.CredentialDialogUser" -Recurse -ErrorAction SilentlyContinue
+        remove-item "Registry::$sid\WavesorSWUpdater.CredentialDialogUser.1.0" -Recurse -ErrorAction SilentlyContinue
+        remove-item "Registry::$sid\WavesorSWUpdater.OnDemandCOMClassUser" -Recurse -ErrorAction SilentlyContinue
+        remove-item "Registry::$sid\WavesorSWUpdater.OnDemandCOMClassUser.1.0" -Recurse -ErrorAction SilentlyContinue
+        remove-item "Registry::$sid\WavesorSWUpdater.PolicyStatusUser" -Recurse -ErrorAction SilentlyContinue
+        remove-item "Registry::$sid\WavesorSWUpdater.PolicyStatusUser.1.0" -Recurse -ErrorAction SilentlyContinue
+        remove-item "Registry::$sid\WavesorSWUpdater.Update3COMClassUser" -Recurse -ErrorAction SilentlyContinue
+        remove-item "Registry::$sid\WavesorSWUpdater.Update3COMClassUser.1.0" -Recurse -ErrorAction SilentlyContinue
+        remove-item "Registry::$sid\WavesorSWUpdater.Update3WebUser" -Recurse -ErrorAction SilentlyContinue
+        remove-item "Registry::$sid\WavesorSWUpdater.Update3WebUser.1.0" -Recurse -ErrorAction SilentlyContinue
     }
 }
 
